bbaa25456ea4b948318d09af7f36dc93eab205b8
noonbbara <149371593+noonbbara@users.noreply.github.com>
2024-10-29 02:28:29 +0900

    [INITIAL]

---
+++ src/test/java/yoong/yoongmantle/YoongmantleApplicationTests.java
@@ -0,0 +1,13 @@
+package yoong.yoongmantle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoongmantleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
